3d23fdf19394d68d76200f1d06c33f7a4a9519dd
capriciousbased <mdanisulhoquekhan@gmail.com>
2021-03-18 20:23:43 +0100

    added about us

--- src/components/pages/AboutUs.js
+++ src/components/pages/AboutUs.js
@@ -1,6 +1,22 @@
 import React from "react";
-import "../../App.css";
+import "./AboutUs.css";
+import missionImg from "../../images/mission.svg";
+import teamImg from "../../images/meet-team.png";
 
 export default function AboutUs() {
-  return <h1 className="about"> About Us</h1>;
+  return (
+    <div class="about-us">
+      <div className="mission">
+        <img className="mission-img" src={missionImg} alt="" />
+        <h2 className="mission-heading"> Our Mission </h2>
+        <p className="mission-text">
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Veniam
+          necessitatibus velit debitis reiciendis unde voluptatibus quae a
+          delectus, eum, magni consectetur asperiores. Facere debitis ducimus,
+          eaque consequuntur quam ipsum maiores.
+        </p>
+      </div>
+      <img className="team-img" src={teamImg} alt="" />
+    </div>
+  );
 }
