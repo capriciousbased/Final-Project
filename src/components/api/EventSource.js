2d339e613ef74035c47ad3ee7861b417be1c4db1
capriciousbased <mdanisulhoquekhan@gmail.com>
2021-03-20 20:48:54 +0100

    update api key

--- src/components/api/EventSource.js
+++ src/components/api/EventSource.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "https://my-events-api-project.herokuapp.com/events",
+  baseURL: "https://motivity-event-api.herokuapp.com/events",
 });
