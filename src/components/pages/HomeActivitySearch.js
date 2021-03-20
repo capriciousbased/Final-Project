8a426fff22f532aedf4a36d95201427d4329e55f
capriciousbased <mdanisulhoquekhan@gmail.com>
2021-03-20 03:08:42 +0100

    editet home search button

--- src/components/pages/HomeActivitySearch.js
+++ src/components/pages/HomeActivitySearch.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./HomeActivitySearch.css";
 import SearchImgUrl from "../../images/search.svg";
+import Button from '@material-ui/core/Button';
 
 function HomeActivitySearch() {
   return (
@@ -8,14 +9,17 @@ function HomeActivitySearch() {
       <img className="search_img" src={SearchImgUrl} alt="img1" />
       <h2>What are you waiting for? Get, set, go!</h2>
       <div className="searchArea">
-        <input
+        {/* <input
           className="home__searchInput"
           type="text"
           placeholder="Find Activities"
         />
         <button type="submit" className="search__btn">
           Search
-        </button>
+        </button> */}
+        <Button variant="contained" color="primary" href="/find-activity">
+        Search
+      </Button>
       </div>
     </div>
   );
