aa16b3edd19ab484be133bdf218e464fcb353048
capriciousbased <mdanisulhoquekhan@gmail.com>
2021-03-30 22:34:42 +0200

    bug fixed

---
+++ build/precache-manifest.f6b3cd02314eb281cd5f12db0cfd87b4.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b01bd42534ec50897a9e91abc18fae45",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c1940f6ed022e46add19",
+    "url": "/static/css/main.4f781903.chunk.css"
+  },
+  {
+    "revision": "0de35ee6059977be1f84",
+    "url": "/static/js/2.b872d6e8.chunk.js"
+  },
+  {
+    "revision": "beb32f0a96a8b060a5850315d4c8a68e",
+    "url": "/static/js/2.b872d6e8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c1940f6ed022e46add19",
+    "url": "/static/js/main.fd5ed63a.chunk.js"
+  },
+  {
+    "revision": "355b6d8b4eaedb313daa",
+    "url": "/static/js/runtime-main.34f062af.js"
+  },
+  {
+    "revision": "d0b57086eda9d418dcff4e2cc9ba87e7",
+    "url": "/static/media/activity-src.d0b57086.svg"
+  },
+  {
+    "revision": "9b269fe2e20a99cc9d05dd5722cfb090",
+    "url": "/static/media/activity.9b269fe2.svg"
+  },
+  {
+    "revision": "6da978bda98d758cc280eba004ac6b4d",
+    "url": "/static/media/img-1.6da978bd.svg"
+  },
+  {
+    "revision": "7c23b226bb26c1ef9a117c9adda02996",
+    "url": "/static/media/img-2.7c23b226.svg"
+  },
+  {
+    "revision": "63249ea2ad2d6fd52100a9b5113e6820",
+    "url": "/static/media/login.63249ea2.jpg"
+  },
+  {
+    "revision": "2412e7d756317d91a4c4297dc92735fc",
+    "url": "/static/media/logos.2412e7d7.svg"
+  },
+  {
+    "revision": "a637a8b7483cbe66a790f31866bc9ad3",
+    "url": "/static/media/meet-team.a637a8b7.png"
+  },
+  {
+    "revision": "d63b75aa55cf15690175143376960c60",
+    "url": "/static/media/mission.d63b75aa.svg"
+  },
+  {
+    "revision": "903d76afa564a0d853d78b1c9df7c5d4",
+    "url": "/static/media/search.903d76af.svg"
+  },
+  {
+    "revision": "e9139022e690cb1dab4a7b716ae2e916",
+    "url": "/static/media/signup.e9139022.jpg"
+  }
+]);
--- src/components/pages/SignUp.js
+++ src/components/pages/SignUp.js
@@ -17,7 +17,7 @@ function Copyright() {
   return (
     <Typography variant="body2" color="textSecondary" align="center">
       {"Copyright © "}
-      <Link color="inherit" href="https://material-ui.com/">
+      <Link color="inherit" href="">
         Motivity
       </Link>{" "}
       {new Date().getFullYear()}
