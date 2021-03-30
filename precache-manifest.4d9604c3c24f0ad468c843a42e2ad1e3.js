8bb527d9da64bab1f556b707b3a8c72c3600c542
capriciousbased <mdanisulhoquekhan@gmail.com>
2021-03-30 19:21:51 +0200

    Updates

---
+++ precache-manifest.4d9604c3c24f0ad468c843a42e2ad1e3.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c919c9064acd70c6316d5ce39b4176f3",
+    "url": "/Final-Project/index.html"
+  },
+  {
+    "revision": "52372205ececcb21c64e",
+    "url": "/Final-Project/static/css/main.96a7eebd.chunk.css"
+  },
+  {
+    "revision": "0de35ee6059977be1f84",
+    "url": "/Final-Project/static/js/2.b872d6e8.chunk.js"
+  },
+  {
+    "revision": "beb32f0a96a8b060a5850315d4c8a68e",
+    "url": "/Final-Project/static/js/2.b872d6e8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "52372205ececcb21c64e",
+    "url": "/Final-Project/static/js/main.b3e6cac5.chunk.js"
+  },
+  {
+    "revision": "60a6167848a3f6907f30",
+    "url": "/Final-Project/static/js/runtime-main.86264dc4.js"
+  },
+  {
+    "revision": "d0b57086eda9d418dcff4e2cc9ba87e7",
+    "url": "/Final-Project/static/media/activity-src.d0b57086.svg"
+  },
+  {
+    "revision": "9b269fe2e20a99cc9d05dd5722cfb090",
+    "url": "/Final-Project/static/media/activity.9b269fe2.svg"
+  },
+  {
+    "revision": "6da978bda98d758cc280eba004ac6b4d",
+    "url": "/Final-Project/static/media/img-1.6da978bd.svg"
+  },
+  {
+    "revision": "7c23b226bb26c1ef9a117c9adda02996",
+    "url": "/Final-Project/static/media/img-2.7c23b226.svg"
+  },
+  {
+    "revision": "63249ea2ad2d6fd52100a9b5113e6820",
+    "url": "/Final-Project/static/media/login.63249ea2.jpg"
+  },
+  {
+    "revision": "2412e7d756317d91a4c4297dc92735fc",
+    "url": "/Final-Project/static/media/logos.2412e7d7.svg"
+  },
+  {
+    "revision": "a637a8b7483cbe66a790f31866bc9ad3",
+    "url": "/Final-Project/static/media/meet-team.a637a8b7.png"
+  },
+  {
+    "revision": "d63b75aa55cf15690175143376960c60",
+    "url": "/Final-Project/static/media/mission.d63b75aa.svg"
+  },
+  {
+    "revision": "903d76afa564a0d853d78b1c9df7c5d4",
+    "url": "/Final-Project/static/media/search.903d76af.svg"
+  },
+  {
+    "revision": "e9139022e690cb1dab4a7b716ae2e916",
+    "url": "/Final-Project/static/media/signup.e9139022.jpg"
+  }
+]);
